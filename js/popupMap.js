5fd384a1b48a6afd39c73241c99c05f263037d2c
Max Guravlev <max.kharkov888@gmail.com>
2022-12-18 18:28:37 +0200

    addMaps

--- js/popupMap.js
+++ js/popupMap.js
@@ -4,19 +4,19 @@ var mapLink = document.querySelector(".contacts-button-map");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-close");
 
-mapLink.addEventListener("click", function (evt) {
-	evt.preventDefault();
+mapLink.addEventListener("click", function (evnt) {
+	evnt.preventDefault();
 	mapPopup.classList.add("modal-show");
 });
 
-mapClose.addEventListener("click", function (evt) {
-	evt.preventDefault();
+mapClose.addEventListener("click", function (evnt) {
+	evnt.preventDefault();
 	mapPopup.classList.remove("modal-show");
 });
 
-window.addEventListener("keydown", function (evt) {
-	evt.preventDefault();
-	if (evt.keyCode === 27) {
+window.addEventListener("keydown", function (evtn) {
+	evnt.preventDefault();
+	if (evnt.keyCode === 27) {
 		if (mapPopup.classList.contains("modal-show")) {
 			mapPopup.classList.remove("modal-show");
 		}
